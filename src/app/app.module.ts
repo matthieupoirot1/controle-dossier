b1babe9272885a35974c565f3821278230c8ceb8
Matthieu Poirot <matthieu.poirot1@gmail.com>
2021-04-30 12:31:45 +0200

    homepage UI revamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { AppComponent } from './app.component';
 import { EditFolderComponent } from './folders/comp/edit-folder/edit-folder.component';
 import { CreateFolderComponent } from './folders/comp/create-folder/create-folder.component';
 import { ListFoldersComponent } from './folders/comp/list-folders/list-folders.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -16,12 +22,18 @@ import { ListFoldersComponent } from './folders/comp/list-folders/list-folders.c
     CreateFolderComponent,
     ListFoldersComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      HttpClientModule,
+      ReactiveFormsModule,
+      BrowserAnimationsModule,
+      MatCardModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatDividerModule,
+      MatGridListModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
